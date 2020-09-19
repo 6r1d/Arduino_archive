e3e874b453fc59ac0354d3d14fbef6895d367e31
Victor Gridnevsky <vic.6r1d@gmail.com>
2020-09-19 19:22:49 +0300

    Adds a base Mozzi library example

---
+++ STM32_F1_Blue_Pill/Mozzi_base_example/Mozzi_base_example.ino
@@ -0,0 +1,23 @@
+#include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/triangle2048_int8.h>
+#include <STM32ADC.h>
+
+#define CONTROL_RATE 128
+Oscil <2048, AUDIO_RATE> aTri(TRIANGLE2048_DATA);
+
+void setup() {
+  aTri.setFreq(440);
+  startMozzi(CONTROL_RATE);
+}
+
+void updateControl() {
+}
+
+int updateAudio() {
+  return aTri.next();
+}
+
+void loop() {
+  audioHook();
+}
